91e5b7e2947a04a2f85f534d1f9f2c737b294ab4
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-09-22 02:00:31 +1000

    docs: added dummy questions data to mimic data that will be fetched from API

---
+++ src/questionsData.js
@@ -0,0 +1,38 @@
+export const questionsData = [
+  {
+    id: 1,
+    question: "How would one say goodbye in Spanish?",
+    options: ["Adios", "Hola", "Au Revoir", "Salir"],
+    correctAnswer: "Adios",
+  },
+  {
+    id: 2,
+    question:
+      "Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?",
+    options: [
+      "Cabbage Patch Kids",
+      "Transformers",
+      "Care Bears",
+      "Rubik's Cube",
+    ],
+    correctAnswer: "Cabbage Patch Kids",
+  },
+  {
+    id: 3,
+    question: "What is the hottest planet in our Solar System?",
+    options: ["Mercury", "Venus", "Mars", "Saturn"],
+    correctAnswer: "Venus",
+  },
+  {
+    id: 4,
+    question: "In which country was the caesar salad invented?",
+    options: ["Italy", "Portugal", "Mexico", "France"],
+    correctAnswer: "Mexico",
+  },
+  {
+    id: 5,
+    question: "How Many Hearts Does An Octopus Have?",
+    options: ["One", "Two", "Three", "Four"],
+    correctAnswer: "Three",
+  },
+];
