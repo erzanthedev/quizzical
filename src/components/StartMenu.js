72229c6c9b6da35ba6e15e29eea6366c21268de9
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-10-01 02:02:21 +1000

    fix: changed tag for start menu

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -3,7 +3,9 @@ export default function StartMenu(props) {
   return (
     <div className="start-menu">
       <h1 className="start-menu-title">Quizzical</h1>
-      <p className="start-menu-text">Some description if needed</p>
+      <p className="start-menu-text">
+        Test Your Knowledge, One Question at a Time
+      </p>
       <Button classname="start-menu-btn" handleRoute={props.handleRoute}>
         Start quiz
       </Button>
